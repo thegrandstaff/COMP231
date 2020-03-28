ae1604f8a91b27727aa0ef990cea718f33588037
thegrandstaff <59798435+thegrandstaff@users.noreply.github.com>
2020-03-28 10:58:35 -0400

    Add files via upload

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,121 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.IO;
+
+namespace PatientList
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow 
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            LoadListOfPatients();
+
+            //navigation bar (combobox)
+            patientListNav.Items.Add("Sign Up");
+        }
+
+        ObservableCollection<Patient> Patients = new ObservableCollection<Patient>();
+
+        // patientList -> for putting the patient objects into the Data Grid
+        ListOfPatients patientList = new ListOfPatients();
+
+        private void LoadListOfPatients()
+        {
+            //patient list datagrid
+            /*
+            patientList.Add(new Patient { FirstName = "John", LastName = "Doe", Email = "john.doe@hotmail.com", Phone = "416-888-8238", Address = "50 Center Drive", Password = "Secret123$" });
+            patientList.Add(new Patient { FirstName = "Jane", LastName = "Dot", Email = "jane.dot@hotmail.com", Phone = "416-801-1235", Address = "109 Tender Road", Password = "Secret456$" });
+            patientList.Add(new Patient { FirstName = "Jack", LastName = "Daniels", Email = "jack_daniels@hotmail.com", Phone = "416-801-1235", Address = "109 Tender Road"});
+            patientList.Add(new Patient { FirstName = "Peter", LastName = "Parker", Email = "spider-man@hotmail.com", Phone = "416-345-4567", Address = "23 Daily Drive", Password = "Spider111&" });
+            */
+
+            //hard coded objects for demo
+            Patients.Add(new Patient {
+                FirstName = "John",
+                LastName = "Doe",
+                Email = "john.doe@hotmail.com",
+                Phone = "416-888-8238",
+                Address = "50 Center Drive",
+                Password = "Secret123$"
+            });
+
+            Patients.Add(new Patient
+            {
+                FirstName = "Jane",
+                LastName = "Dot",
+                Email = "jane.dot@hotmail.com",
+                Phone = "416-801-1235",
+                Address = "109 Tender Road",
+                Password = "Secret456$"
+            });
+
+            Patients.Add(new Patient
+            {
+                FirstName = "Jack",
+                LastName = "Daniels",
+                Email = "jack_daniels@hotmail.com",
+                Phone = "416-801-1235",
+                Address = "109 Tender Road"
+            });
+
+            Patients.Add(new Patient
+            {
+                FirstName = "Peter",
+                LastName = "Parker",
+                Email = "spider-man@hotmail.com",
+                Phone = "416-345-4567",
+                Address = "23 Daily Drive",
+                Password = "Spider111&"
+            });
+
+            //add patient objects to the patient list
+            for (int i = 0; i < Patients.Count; i++)
+            {
+                Patient account = Patients[i];
+                patientList.Add(new Patient 
+                {
+                    FirstName = account.FirstName, 
+                    LastName = account.LastName, 
+                    Address = account.Address, 
+                    Phone = account.Phone
+                });
+            }
+            
+            //take patient list into the data grid
+            dg_patients.ItemsSource = patientList;
+            
+        }
+
+        private void dg_patients_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+
+        }
+
+        private void patientListNav_SelectedIndexChanged(object sender, SelectionChangedEventArgs e)
+        {
+            if (patientListNav.SelectedIndex == 0) //sign up
+            {
+
+            }
+
+        }
+
+    }
+}
---
+++ Patient.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PatientList
+{
+    public class Patient : INotifyPropertyChanged
+    {
+        //declare variables
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Email { get; set; }
+        public string Phone { get; set; }
+        public string Address { get; set; }
+        public string Password { get; set; }
+
+        //patient constructor
+        
+        public Patient(string fName, string lName, string email, string phone, string address, string password)
+        {
+            this.FirstName = fName;
+            this.LastName = lName;
+            this.Email = email;
+            this.Phone = phone;
+            this.Address = address;
+            this.Password = password;
+        }
+        
+        
+        public Patient(string fName, string lName, string phone, string address)
+        {
+            this.FirstName = fName;
+            this.LastName = lName;
+            this.Phone = phone;
+            this.Address = address;
+        }
+
+        public Patient()
+        { 
+        
+        }
+
+        //Patient p1 = new Patient("John", "Doe", "john.doe@hotmail.com", "416-888-8238", "50 Center Drive", "Secret123$");
+
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        // Create OnPropertyChanged method to raise the event
+        protected void OnPropertyChanged(string propertyName)
+        {
+            PropertyChangedEventHandler handler = PropertyChanged;
+            if (handler != null)
+            {
+                handler(this, new PropertyChangedEventArgs(propertyName));
+            }
+        }
+
+    }
+}
