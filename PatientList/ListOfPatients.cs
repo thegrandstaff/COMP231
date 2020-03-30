eaaeffff03dd33a7af8dd4a7b295d4ee78568d8a
thegrandstaff <59798435+thegrandstaff@users.noreply.github.com>
2020-03-29 20:18:21 -0400

    Update ListOfPatients.cs

--- PatientList/ListOfPatients.cs
+++ PatientList/ListOfPatients.cs
@@ -7,6 +7,7 @@ using System.Threading.Tasks;
 
 namespace PatientList
 {
+    // class used to create the list of patients (observablecollections)
     class ListOfPatients : ObservableCollection<Patient>
     {
         public ListOfPatients() : base()
